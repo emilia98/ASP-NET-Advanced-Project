2c441a35341c64b81510500c47b935001bb34817
emilia98 <emilia_nedialkova@abv.bg>
2024-04-20 22:03:04 +0300

    init project

---
+++ Workspacer/Web/Workspacer.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Workspacer.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
