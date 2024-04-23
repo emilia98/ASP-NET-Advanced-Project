78f4aafd7886da2da552e1491cd7f7c823df66cb
emilia98 <emilia_nedialkova@abv.bg>
2024-04-23 23:41:33 +0300

    add ApplicationDbContext

---
+++ Workspacer/Data/Workspacer.Data/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Workspacer.Data;
+using Workspacer.Data.Models;
+
+namespace Workspacer.Data
+{
+	public class ApplicationDbContext
+        : IdentityDbContext<ApplicationUser, ApplicationRole, int, IdentityUserClaim<int>, ApplicationUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+        // DBSETS HERE
+        public DbSet<Request> Requests { get; set; }
+
+        public DbSet<Offer> Offers { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var entityTypes = modelBuilder.Model.GetEntityTypes().ToList();
+            var foreignKeys = entityTypes
+                .SelectMany(e => e.GetForeignKeys().Where(fk => fk.DeleteBehavior == DeleteBehavior.Cascade));
+
+            foreach (var foreignKey in foreignKeys)
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+
+            modelBuilder.Entity<ApplicationRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+
+        }
+
+    }
+}
